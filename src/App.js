4848da3a9cf0b0ddef16be60cc447baade8516d9
Gregory Delgado <gregd92694@yahoo.com>
2022-08-18 21:21:57 -0500

    Created as react App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './style.css'
-
+import logo from './logo.svg';
+import './App.css';
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
+function App() {
+  const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-})
-
+const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(5);
+document.body.innerHTML='';
+document.body.appendChild(renderer.domElement);
 
 const boxGeometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color:0x00ff00, wireframe: true});
@@ -20,3 +20,9 @@ const box = new THREE.Mesh(boxGeometry,material);
 scene.add(box);
 
 renderer.render(scene,camera);
+  return (
+null
+  );
+}
+
+export default App;
