c85d74d03b6bc89da864e25f84530c1fc729cdbc
Gregory Delgado <gregd92694@yahoo.com>
2022-08-19 21:26:53 -0500

    Orbit controls added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import {Canvas, useFrame} from 'react-three-fiber';
+import {Canvas, useFrame, useThree, extend} from 'react-three-fiber';
 import {useRef} from 'react';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+extend({OrbitControls});
+
+const Orbit=()=>{
+  const{camera,gl}=useThree();
+  return(
+    <orbitControls args={[camera, gl.domElement]}/>
+  )
+}
 
 
 const Box = ()=>{
@@ -22,8 +30,10 @@ function App() {
 
   return (
   <div style ={{height:'100vh', width:'100vw'}}>
-    <Canvas style={{background: 'black'}}>
-    <Box/>
+    <Canvas style={{background: 'black'}} camera ={{position:[3,3,3]}}>
+      <Orbit/>
+    {/* <Box/> */}
+    <axesHelper args={[5]}/>
     </Canvas>
     </div>
   );
