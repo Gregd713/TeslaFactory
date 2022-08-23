195ea86c21433e1fb04e7b6d5e792a381881fcf4
Gregory Delgado <gregd92694@yahoo.com>
2022-08-22 21:08:45 -0500

    orbit dragable functionality added

--- src/components/Orbit.jsx
+++ src/components/Orbit.jsx
@@ -5,7 +5,9 @@ extend({OrbitControls});
 const Orbit=()=>{
     const{camera,gl}=useThree();
     return(
-      <orbitControls args={[camera, gl.domElement]}/>
+      <orbitControls 
+      attach ='orbitControls'
+      args={[camera, gl.domElement]}/>
     )
   }
 
