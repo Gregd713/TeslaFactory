23e9ce77b3d1f42ccd7da012c922a91300470b6c
Gregory Delgado <gregd92694@yahoo.com>
2022-08-23 17:18:12 -0500

    drag function added

--- src/components/Dragable.jsx
+++ src/components/Dragable.jsx
@@ -7,23 +7,42 @@ extend({DragControls});
 
 
 const Dragable=props=>{
-    const groupRef=useRef();
-    const controlsRef = useRef()
-    const [children, setChildren] =useState([])
-    const {camera, gl,scene}=useThree();
-    useEffect(()=>{
+    const groupRef = useRef();
+    const controlsRef = useRef();
+    const [children, setChildren] = useState([])
+    const { camera, gl, scene } = useThree();
+
+    useEffect(() => {
         setChildren(groupRef.current.children)
-     
     },[])
 
-    useEffect(()=>{
-        controlsRef.current.addEventListener('hoveron',
-        e=> scene.orbitControls.enabled=false
-        );
-        controlsRef.current.addEventListener('hoveroff',
-        e=> scene.orbitControls.enabled=true
-        );
-
+    useEffect(() => {
+        controlsRef.current.addEventListener(
+            'hoveron',
+            e => scene.orbitControls.enabled = false
+        )
+        controlsRef.current.addEventListener(
+            'hoveroff',
+            e => scene.orbitControls.enabled = true
+        )
+        controlsRef.current.addEventListener(
+            'dragstart',
+            e => {
+                console.log(e.object);
+                e.object.api?.mass.set(0)
+            }
+        )
+        controlsRef.current.addEventListener(
+            'dragend',
+            e => e.object.api?.mass.set(1)
+        )
+        controlsRef.current.addEventListener(
+            'drag',
+            e => {
+                e.object.api?.position.copy(e.object.position)
+                e.object.api?.velocity.set(0,0,0)
+            }
+        )
     },[children])
 
     return(
