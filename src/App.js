ce044da45e6224746c97f33f0e0afa8de0ea25ac
Gregory Delgado <gregd92694@yahoo.com>
2022-08-22 20:07:55 -0500

    Dragable functionality added

--- src/App.js
+++ src/App.js
@@ -1,63 +1,35 @@
 import './App.css';
-import {Canvas,  } from 'react-three-fiber';
-import {Suspense, } from 'react';
+import {Canvas} from 'react-three-fiber';
+import {Suspense} from 'react';
 import Orbit from './components/Orbit';
 import Box from './components/Box';
 import Background from './components/Background';
 import Floor from './components/Floor';
-import Bulb from './components/Bulb'
-import * as THREE from 'three';
-import { PointLight, Scene } from 'three';
-
-
-
-
-
-
-
-
-
-
-
+import Bulb from './components/Bulb';
+import ColorPicker from './components/ColorPicker';
+import Dragable from './components/Dragable';
 
 function App() {
-  const handleClick =e=>{
-    if(!window.activeMesh) return;
-    window.activeMesh.material.color= new THREE.Color(e.target.style.background)
-  }
+
   return (
   <div style ={{height:'100vh', width:'100vw'}}>
-    <div style={{position:'absolute',zIndex:1}}>
-      <div onClick={handleClick}
-      style={{
-        background:'blue',
-        height:50,
-        width:50}}
-      ></div>
-            <div onClick={handleClick}
-      style={{
-        background:'yellow',
-        height:50,
-        width:50}}
-      ></div>
-            <div onClick={handleClick}
-      style={{
-        background:'white',
-        height:50,
-        width:50}}
-      ></div>
-    </div>
+    <ColorPicker/>
     <Canvas shadows  style={{background: 'black'}} camera ={{position:[7,7,7]}}>
      <Orbit/>
+     <axesHelper args={[5]}/>
+     <Dragable>
      <Suspense fallback={null}>
      {/* <fog attach = 'fog' args={['white',1,10]}/> */}
      <Bulb position={[0,3,0]}/>
      <ambientLight intensity={0.2}/>
     <Box position={[4,1,0]} />
     <Box position={[-4,1,0]} />
+    </Suspense>
+    </Dragable>
+    <Suspense>
     <Background/>
     </Suspense>
-    <axesHelper args={[5]}/>
+
     <Floor position = {[0,-0.5,0]} />
     </Canvas>
     </div>
