2d81bbe44929f1b275bba4de8fc7c25fdebde0a4
Gregory Delgado <gregd92694@yahoo.com>
2022-08-23 17:15:24 -0500

    gltf models added to scene

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import {Suspense} from 'react';
 import Orbit from './components/Orbit';
 import Box from './components/Box';
 import Background from './components/Background';
-import Floor from './components/Floor';
+import Plane from './components/Floor';
 import Bulb from './components/Bulb';
 import ColorPicker from './components/ColorPicker';
 import Dragable from './components/Dragable';
-
+import {Physics} from '@react-three/cannon';
+import Model from './components/Model'
 function App() {
 
   return (
@@ -17,20 +18,38 @@ function App() {
     <Canvas shadows  style={{background: 'black'}} camera ={{position:[7,7,7]}}>
      <Orbit />
      <axesHelper args={[5]}/>
+     <Physics>
      <Dragable>
+      <Suspense>
+        <Model 
+        path='/tesla_model_3/scene.gltf'
+        scale ={new Array(3).fill(0.01)}
+        position={[4,0.1,0]}
+        />
+      </Suspense>
+      <Suspense>
+        <Model 
+        path='/tesla_model_s/scene.gltf'
+        scale ={new Array(3).fill(0.7)}
+        position={[-4,-.3,0]}
+        />
+      </Suspense>
      <Suspense fallback={null}>
      {/* <fog attach = 'fog' args={['white',1,10]}/> */}
      <Bulb position={[0,3,0]}/>
      <ambientLight intensity={0.2}/>
-    <Box position={[4,1,0]} />
-    <Box position={[-4,1,0]} />
+    {/* <Box position={[4,1,0]} />
+    <Box position={[-4,1,0]} /> */}
     </Suspense>
+
     </Dragable>
+    <Plane position = {[0,-0.5,0]} />
+    </Physics>
     <Suspense>
     <Background/>
     </Suspense>
 
-    <Floor position = {[0,-0.5,0]} />
+
     </Canvas>
     </div>
   );
