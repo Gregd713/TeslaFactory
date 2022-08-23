f9acd9a8d36c894c79f5589c5979061584256d7e
Gregory Delgado <gregd92694@yahoo.com>
2022-08-22 21:08:03 -0500

    hoveron hoveroff listeners added

--- src/components/Dragable.jsx
+++ src/components/Dragable.jsx
@@ -8,14 +8,29 @@ extend({DragControls});
 
 const Dragable=props=>{
     const groupRef=useRef();
+    const controlsRef = useRef()
     const [children, setChildren] =useState([])
-    const {camera, gl}=useThree();
+    const {camera, gl,scene}=useThree();
     useEffect(()=>{
         setChildren(groupRef.current.children)
-    })
+     
+    },[])
+
+    useEffect(()=>{
+        controlsRef.current.addEventListener('hoveron',
+        e=> scene.orbitControls.enabled=false
+        );
+        controlsRef.current.addEventListener('hoveroff',
+        e=> scene.orbitControls.enabled=true
+        );
+
+    },[children])
+
     return(
     <group ref= {groupRef}>
-        <dragControls args={[children,camera,gl.domElement]}/>
+        <dragControls 
+        ref ={controlsRef}
+        args={[children,camera,gl.domElement]}/>
     {props.children}
     </group>
     )
