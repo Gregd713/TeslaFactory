f8c41f9e0f44d7b7b30bfee6528fd4f385aa5485
Gregory Delgado <gregd92694@yahoo.com>
2022-09-04 11:14:48 -0500

    state file created for app

---
+++ src/state.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three'
+
+const state={
+    activeMesh:null,
+    cameraPos:new THREE.Vector3(7,7,7),
+    target:new THREE.Vector3(4,0,0),
+    shouldUpdate: true
+}
+
+export default state
