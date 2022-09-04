9455c286eeb9c44360f042a3c08053df49a4b0e7
Gregory Delgado <gregd92694@yahoo.com>
2022-09-04 11:12:35 -0500

    Camera Controls added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './App.css';
 import {Canvas} from 'react-three-fiber';
 import {Suspense} from 'react';
 import Orbit from './components/Orbit';
-import Box from './components/Box';
 import Background from './components/Background';
 import Plane from './components/Floor';
 import Bulb from './components/Bulb';
 import ColorPicker from './components/ColorPicker';
-import Dragable from './components/Dragable';
 import {Physics} from '@react-three/cannon';
-import Model from './components/Model';
-import BoundingBox from './components/BoundinBox';
+import Cars from './components/Cars';
+import CameraControls from './components/CameraControls';
+
 function App() {
 
   return (
@@ -19,49 +18,18 @@ function App() {
     <Canvas shadows  style={{background: 'black'}} camera ={{position:[7,7,7]}}>
      <Orbit />
      <axesHelper args={[5]}/>
+     <CameraControls/>
      <Physics>
-      
-     <Dragable transformGroup>
-     <BoundingBox 
-     position={[4,4,0]}
-     dims={[3,2,6]}
-    offset={[0,-.4,0.8]}
-     >
-      <Suspense>
-        <Model 
-        path='/tesla_model_3/scene.gltf'
-        scale ={new Array(3).fill(0.01)}
-        />
-      </Suspense>
-      </BoundingBox>
-      </Dragable>
-      <Dragable transformGroup>
-      <BoundingBox 
-      position={[-4,4,0]}
-      dims={[3,2,6]}
-      offset={[0,-.8,0.2]}
-      >
-      <Suspense>
-        <Model 
-        path='/tesla_model_s/scene.gltf'
-        scale ={new Array(3).fill(0.7)}
-        />
-      </Suspense>
-      </BoundingBox>
-      </Dragable>
      <Suspense fallback={null}>
      <Bulb position={[0,3,0]}/>
-
+     <Cars/>
      <ambientLight intensity={0.2}/>
     </Suspense>
-   
     <Plane position = {[0,-0.5,0]} />
     </Physics>
     <Suspense>
     <Background/>
     </Suspense>
-
-
     </Canvas>
     </div>
   );
